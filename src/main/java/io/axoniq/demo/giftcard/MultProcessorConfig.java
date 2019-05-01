885ea8fe9d1826aba26be75ae90baba98cd31088
gjmwoods <gregory.woods@axoniq.io>
2019-05-01 15:07:53 +0200

    Initial setup

--- src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
+++ src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
@@ -1,6 +1,9 @@
 package io.axoniq.demo.giftcard;
 
 import io.axoniq.demo.giftcard.query.CardSummaryProjection;
+import org.axonframework.common.jdbc.PersistenceExceptionResolver;
+import org.axonframework.common.jpa.EntityManagerProvider;
+import org.axonframework.common.transaction.TransactionManager;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventhandling.MultiStreamableMessageSource;
 import org.axonframework.eventhandling.TrackedEventMessage;
@@ -8,34 +11,58 @@ import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
+import org.axonframework.eventsourcing.eventstore.jpa.JpaEventStorageEngine;
 import org.axonframework.messaging.StreamableMessageSource;
+import org.axonframework.serialization.Serializer;
 import org.axonframework.spring.config.AxonConfiguration;
+import org.axonframework.springboot.util.RegisterDefaultEntities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.sql.SQLException;
 import java.util.HashMap;
+import javax.sql.DataSource;
 
 @Configuration
+@RegisterDefaultEntities(packages = {
+        "org.axonframework.eventsourcing.eventstore.jpa"
+})
 public class MultProcessorConfig {
 
-    @Bean
     @Primary
     @Qualifier("permanent")
-    public EventStorageEngine configureEventStoreForPermanentEvents(){
-        return new InMemoryEventStorageEngine();
-    }
-
     @Bean
-    @Qualifier("permanent")
-    public EmbeddedEventStore eventStore(EventStorageEngine storageEngine, AxonConfiguration configuration) {
+    public EmbeddedEventStore permanentEventStore(
+            @Qualifier("permanent") EventStorageEngine storageEngine, AxonConfiguration configuration) {
         return EmbeddedEventStore.builder()
                                  .storageEngine(storageEngine)
                                  .messageMonitor(configuration.messageMonitor(EventStore.class, "eventStore"))
                                  .build();
     }
+
+
+    @Primary
+    @Qualifier("permanent")
+    @Bean
+    public JpaEventStorageEngine eventStorageEngine(Serializer eventSerializer,
+                                                    Serializer snapshotSerializer,
+                                                    DataSource dataSource,
+                                                    EntityManagerProvider entityManagerProvider,
+                                                    TransactionManager transactionManager) throws SQLException {
+        return JpaEventStorageEngine.builder()
+                                    .eventSerializer(eventSerializer)
+                                    .snapshotSerializer(snapshotSerializer)
+                                    .dataSource(dataSource)
+                                    .entityManagerProvider(entityManagerProvider)
+                                    .transactionManager(transactionManager)
+                                    .build();
+    }
+
+
+
 
     @Bean
     @Qualifier("ephemeral")
@@ -54,7 +81,7 @@ public class MultProcessorConfig {
     }
 
     @Bean
-    public MultiStreamableMessageSource multiStreamableMessageSource(@Qualifier("eventStore") EmbeddedEventStore permanentEventStore, @Qualifier("ephemeral") EmbeddedEventStore ephemeralEventStore){
+    public MultiStreamableMessageSource multiStreamableMessageSource(@Qualifier("permanent") EmbeddedEventStore permanentEventStore, @Qualifier("ephemeral") EmbeddedEventStore ephemeralEventStore){
 
         HashMap<String, StreamableMessageSource<TrackedEventMessage<?>>> messageSources = new HashMap<>();
 
