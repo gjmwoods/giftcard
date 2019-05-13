d91126e211a92f99d7705d02adf1f325e0763d2f
gjmwoods <gregory.woods@axoniq.io>
2019-05-13 14:02:14 +0200

    refactor for use with builder api

--- src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
+++ src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
@@ -1,14 +1,11 @@
 package io.axoniq.demo.giftcard;
 
-import io.axoniq.demo.giftcard.query.CardSummaryProjection;
 import org.axonframework.config.EventProcessingConfigurer;
-import org.axonframework.eventhandling.MultiStreamableMessageSource;
-import org.axonframework.eventhandling.TrackedEventMessage;
+import org.axonframework.eventsourcing.MultiStreamableMessageSource;
 import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.EventStore;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-import org.axonframework.messaging.StreamableMessageSource;
 import org.axonframework.spring.config.AxonConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,7 +13,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
-import java.util.HashMap;
 
 @Configuration
 public class MultProcessorConfig {
@@ -56,12 +52,10 @@ public class MultProcessorConfig {
     @Bean
     public MultiStreamableMessageSource multiStreamableMessageSource(@Qualifier("eventStore") EmbeddedEventStore permanentEventStore, @Qualifier("ephemeral") EmbeddedEventStore ephemeralEventStore){
 
-        HashMap<String, StreamableMessageSource<TrackedEventMessage<?>>> messageSources = new HashMap<>();
-
-        messageSources.put("permanent", permanentEventStore);
-        messageSources.put("ephemeral", ephemeralEventStore);
-
-        return new MultiStreamableMessageSource(messageSources);
+        return MultiStreamableMessageSource.builder()
+                                           .addMessageSource("permanent", permanentEventStore)
+                                           .addMessageSource("ephemeral", ephemeralEventStore)
+                                           .build();
     }
 
     @Autowired
