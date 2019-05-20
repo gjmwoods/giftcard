7941ac15edb01cba7638e6b9e27fa8d17ac26242
gjmwoods <gregory.woods@axoniq.io>
2019-05-20 13:31:07 +0200

    micrometer

--- src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
+++ src/main/java/io/axoniq/demo/giftcard/MultProcessorConfig.java
@@ -1,27 +1,31 @@
 package io.axoniq.demo.giftcard;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
 import org.axonframework.axonserver.connector.AxonServerConfiguration;
 import org.axonframework.axonserver.connector.AxonServerConnectionManager;
 import org.axonframework.axonserver.connector.event.axon.AxonServerEventStore;
 import org.axonframework.config.EventProcessingConfigurer;
 import org.axonframework.eventsourcing.MultiStreamableMessageSource;
-import org.axonframework.eventsourcing.eventstore.EmbeddedEventStore;
-import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
-import org.axonframework.eventsourcing.eventstore.EventStore;
-import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
-import org.axonframework.spring.config.AxonConfiguration;
-import org.axonframework.springboot.util.RegisterDefaultEntities;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.time.Duration;
+
 @Configuration
-@RegisterDefaultEntities(packages = {
-        "org.axonframework.eventsourcing.eventstore.jpa"
-})
 public class MultProcessorConfig {
+
+    @Autowired
+    public void configureTimer(MeterRegistry meterRegistry){
+        Timer.builder("Saga.timer")
+             .maximumExpectedValue(Duration.ofMillis(200))
+             .publishPercentiles(0.1, 0.5, 0.9, 0.95, 0.99) // median and 95th percentile
+             .publishPercentileHistogram()
+             .register(meterRegistry);
+    }
 
     @Primary
     @Qualifier("permanent")
